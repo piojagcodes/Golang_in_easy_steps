661c35a4c918d139f62794f682c9d358787581ff
piojagcodes <piojag@gmail.com>
2023-07-14 09:16:28 +0200

    Inserting statements to output all the bit flag settings

---
+++ go/src/bits/main.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+
+func main() {
+	var flags byte = 10 // Binary 1010 (1x8,0x4,1x2,0x1)
+	fmt.Printf("Flag #1:%v\n", (flags & 1) > 0)
+	fmt.Printf("Flag #2:%v\n", (flags & 2) > 0)
+	fmt.Printf("Flag #3:%v\n", (flags & 4) > 0)
+	fmt.Printf("Flag #4:%v\n", (flags & 8) > 0)
+
+	
+}
