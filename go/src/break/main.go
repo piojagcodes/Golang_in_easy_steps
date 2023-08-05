9d2bd2d3f0efd95c42f8801201c0672af8adce0b
piojagcodes <piojag@gmail.com>
2023-08-05 19:05:23 +0200

    Break statement halts the second and third iterations of the inner loop when the outer loop tries to run it for the second time

--- go/src/break/main.go
+++ go/src/break/main.go
@@ -6,6 +6,10 @@ func main() {
 	for i := 1; i <= 3; i++ {
 		// Inner loop can be inserted here
 		for j := 1; j <= 3; j++ {
+			// Statements to be inserted here.
+			if i == 2 && j == 2 {
+				fmt.Println("Breaks when i=", i, "Breaks when j=", j)
+			}
 			fmt.Println("Running i=", i, "j=", j)
 		}
 	}
