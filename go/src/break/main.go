f76e131a9dca4553df81aba9b1acc4ff12021fcb
piojagcodes <piojag@gmail.com>
2023-08-05 19:10:23 +0200

    Continue statement just skips the second iteration of the inner loop when the outer loop tries to run it for the third time.

--- go/src/break/main.go
+++ go/src/break/main.go
@@ -6,6 +6,11 @@ func main() {
 	for i := 1; i <= 3; i++ {
 		// Inner loop can be inserted here
 		for j := 1; j <= 3; j++ {
+			// Continue statement
+			if i == 3 && j == 2 {
+				fmt.Println("Continues when i=", i, "Continues when j=", j)
+				continue
+			}
 			// Statements to be inserted here.
 			if i == 2 && j == 2 {
 				fmt.Println("Breaks when i=", i, "Breaks when j=", j)
